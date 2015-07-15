ad0f325a85348a0652fc7b95501ffcc23d9601f9
Zach Smith <zsmith12@umd.edu>
2015-07-15 12:53:36 -0400

    Statically link std libs; now useable w/o MinGW
    Once the release with the correct binary is posted, this should now
    be statically linked to the standard libraries and thus useable without a
    MinGW/Cygwin environment.
    
    Untested in practice.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-gcc.exe -Wall -o .\builds\win32\genAWGpattern.exe src\defOptions\defOptions.c src\genBinary\genBinary.c src\driver.c
+gcc.exe -Wall -o .\builds\win32\genAWGpattern.exe src\defOptions\defOptions.c src\genBinary\genBinary.c src\driver.c -static-libgcc -static-libstdc++
 @echo OFF
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
 pause
