def9073d8c1f2d0b6c89a8414615ff68050019fc
Zach Smith <zsmith12@umd.edu>
2015-07-17 12:47:47 -0400

    Added pause for an error from PuTTY Link to go.bat

--- go.bat
+++ go.bat
@@ -3,3 +3,6 @@
 if %errorlevel% neq 0 pause && exit /b %errorlevel%
 @echo ON
 "C:\Program Files\PuTTY\plink.exe" -load AWG < awgOutput_points
+@echo OFF
+if %errorlevel% neq 0 pause && exit /b %errorlevel%
+pause
